7edd36822bbc802141ea496d75a9f1aa840aa840
Felippe Scheidt <felippescheidt@gmail.com>
2025-03-02 11:35:31 -0300

    update scripts

--- s6-apt-signed.sh
+++ s6-apt-signed.sh
@@ -2,7 +2,7 @@
 set -e
 target="Ubuntu 24.04"
 stage="Stage 6"
-echo "$stage - $target Install gh, vscode and chorme"
+echo "$stage - $target Update apt sources and install gh, vscode, chrome"
 display_header() {
     echo "=================================================="
     echo -e "\n\033[1;34m($stage)>> $1\033[0m"
@@ -14,8 +14,7 @@ display_header "Checking sudo access"
 sudo -v
 
 # ============================================================
-display_header "Installing Visual Studio Code"
-# Install dependencies
+display_header "Installing vscode"
 if ! command_exists code; then
     sudo apt install software-properties-common apt-transport-https wget gpg -y
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -23,11 +22,10 @@ if ! command_exists code; then
     rm microsoft.gpg
     # Add the VS Code repository
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-    # Update package cache and install VS Code
     sudo apt update
     sudo apt install code -y
     
-    echo "Installing VS Code Python extensions"    
+    echo "Installing vscode extensions"    
     code --install-extension ms-python.python
     code --install-extension ms-python.vscode-pylance
     code --install-extension ms-toolsai.jupyter
@@ -41,7 +39,6 @@ fi
 display_header "Installing google-chrome"
 if ! command_exists google-chrome; then
     echo "Adding Google Chrome repository..."
-    # Add Google’s signing key
     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome-keyring.gpg
     # Add the Google Chrome repository to sources.list.d
     echo "deb [signed-by=/usr/share/keyrings/google-chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -49,7 +46,7 @@ if ! command_exists google-chrome; then
     sudo apt install -y google-chrome-stable
     # Verify installation
     if command -v google-chrome >/dev/null 2>&1; then
-        echo "✅ chrome installed successfully!"
+        echo "✅ chrome installed!"
     else
         echo "❌ chrome installation failed."
     fi
@@ -67,14 +64,14 @@ if ! command_exists gh; then
     sudo apt install -y gh
     # Verify installation
     if command -v gh >/dev/null 2>&1; then
-        echo "✅ GitHub CLI installed successfully!"
+        echo "✅ GH CLI installed!"
     else
-        echo "❌ Installation failed."
+        echo "❌ GH installation failed."
     fi
 else
-    echo "✅ GitHub CLI is already installed."
+    echo "✅ GH CLI is already installed."
 fi
-
 # ============================================================
+
 echo "✅ $stage"
 echo "✅ $stage - $(date '+%Y-%m-%d %H:%M:%S')" >> log.info
--- s7-flatpak.sh
+++ s7-flatpak.sh
@@ -2,7 +2,7 @@
 set -e
 target="Ubuntu 24.04"
 stage="Stage 7"
-echo "$stage - $target Install flatpak"
+echo "$stage - $target - Install flatpak"
 display_header() {
     echo "=================================================="
     echo -e "\n\033[1;34m($stage)>> $1\033[0m"
@@ -18,7 +18,9 @@ display_header "Installing flatpak"
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-display_header "Installing apps"
+# ============================================================
+display_header "Installing flatpak apps"
+
 flatpak install flathub com.calibre_ebook.calibre
 flatpak install flathub com.github.jeromerobert.pdfarranger
 # flatpak install flathub org.gnome.Boxes
